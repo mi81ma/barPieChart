cd180b964cc45db2492cccdbc7547d5c3c4f4395
masato <masato@m-mbp.local>
2018-07-31 18:29:44 +0800

    Initial Commit

---
+++ barPieChart/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  barPieChart
+//
+//  Created by masato on 31/7/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController, ChartViewDelegate {
+    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        setChart()
+        
+    }
+    
+    func setChart() {
+        
+        // Input Data
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        
+        
+        // LineChart
+        var lineChartEntries: [ChartDataEntry] = Array()
+        
+        for i in 0..<months.count {
+            let lineChartEntry = ChartDataEntry(x: Double(i), y: unitsSold[i])
+            lineChartEntries.append(lineChartEntry)
+        }
+        
+        
+        let lineChartDataSet = LineChartDataSet(values: lineChartEntries, label: "Units Sold")
+
+        let lineChartData = LineChartData(dataSet: lineChartDataSet)
+        lineChartView.data = lineChartData
+        
+        
+        // PieChart
+        var pieEntries: [PieChartDataEntry] = Array()
+        
+        for i in 0..<months.count {
+            let pieEntry = PieChartDataEntry(value: unitsSold[i], label: months[i])
+            pieEntries.append(pieEntry)
+        }
+        
+        
+        let pieChartDataSet = PieChartDataSet(values: pieEntries, label: "\(months)")
+        
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        
+        // PieChartColor
+        var colors: [UIColor] = []
+        
+        for _ in 0..<months.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+        
+    }
+    
+}
